7c05c063cdae3771c1b11a6a9d19a9de47162f7f
agapovaelyne <agapova.lina@gmail.com>
2021-08-18 19:30:25 +0300

    Safe removing feature added

--- src/main/java/org/example/model/Post.java
+++ src/main/java/org/example/model/Post.java
@@ -1,10 +1,18 @@
 package org.example.model;
 
+import java.time.LocalDateTime;
+
 public class Post {
     private long id;
     private String content;
+    private LocalDateTime removed = null;
 
     public Post() {
+    }
+
+    public Post(long id, String content) {
+        this.id = id;
+        this.content = content;
     }
 
     public Post(String content) {
@@ -26,4 +34,17 @@ public class Post {
     public void setContent(String content) {
         this.content = content;
     }
+
+    public LocalDateTime getRemoved() {
+        return removed;
+    }
+
+    public void setRemoved(LocalDateTime removed) {
+        this.removed = removed;
+    }
+
+    public boolean isRemoved() {
+        return removed != null;
+    }
+
 }
